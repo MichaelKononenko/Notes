8f0c8cfc3a935ab1070a56fc495881354269d05a
Michael <michael.kononenko101@gmail.com>
2022-09-17 18:42:44 +0300

    started a new idea

--- src/js/mk-experiments.js
+++ src/js/mk-experiments.js
@@ -1,1 +1,19 @@
-//много страшного и непонятного кода но шоб работало😆
+const addButton = document.getElementById('add');
+const notesArea = document.querySelector('.content-wrapper');
+
+console.log(notesArea);
+addButton.addEventListener('click', addItem);
+
+let idCounter = 0;
+
+function addItem() {
+  const newNote = document.createElement('li');
+  newNote.classList.add('note');
+  newNote.id = `item-${idCounter}`;
+  newNote.innerHTML += `
+   <h2 class="note__title">Title</h2>
+    <p class="note__tags">tags</p>
+  `;
+  notesArea.append(newNote);
+  idCounter += 1;
+}
