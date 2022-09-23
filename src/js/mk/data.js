6a5d51190d9028804d69f7dc10a2420e960ee845
Michael <michael.kononenko101@gmail.com>
2022-09-24 00:28:34 +0300

    add data storage and update rendering algorithm

---
+++ src/js/mk/data.js
@@ -0,0 +1,20 @@
+export const notesData = [
+  {
+    title: 'loop',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, maxime!',
+    tags: ['for', 'while', 'do while'],
+  },
+  {
+    title: 'logical operator',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, maxime!',
+    tags: ['&&', '||', '!'],
+  },
+  {
+    title: 'conditional branch',
+    description:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, maxime!',
+    tags: ['if', 'if else', 'else'],
+  },
+];
